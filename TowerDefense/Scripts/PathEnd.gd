27c3215ea183ce8b7aabd61fbae57fe117aeb618
Caleb Cholod <123680320+PurpleRat5@users.noreply.github.com>
2024-06-18 10:16:37 -0400

    Add files via upload
    Added spawners for enemies
    Added placing towers with right click
    Made bullets work better (they still push the enemy)

---
+++ TowerDefense/Scripts/PathEnd.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var HealthText = self.get_parent().get_node("HealthText")
+var MaxHealth = 10
+var Health = MaxHealth
+
+func AreaEntered(area):
+	if area.get_meta("AreaType") == "Movement" and area.get_parent().get_meta("ObjectType") == "Enemy":
+		var Damage = area.get_parent().get_meta("Damage")
+		area.get_parent().queue_free()
+		Health -= Damage
+		print(Health)
+		HealthText.text = str("Health: ", Health, "/", MaxHealth)
